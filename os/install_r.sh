43c80da1c9a711a60573c5d62921b5a480eaa280
Brendan Rocks <rocks.brendan@gmail.com>
2015-05-23 17:44:33 -0700

    R installation scripts

---
+++ os/install_r.sh
@@ -0,0 +1,25 @@
+## R
+
+# At the time of writing, this is required for git2r, which is required for
+# devtools, which is required for quite a lot of your workflow
+sudo apt-get install libssl-dev
+
+
+sudo add-apt-repository -y "deb http://cran.rstudio.com/bin/linux/ubuntu `lsb_release -cs`/"
+# The Ubuntu archives on CRAN are signed with the key of “Michael Rutter
+# marutter@gmail.com”
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+
+sudo apt-get update
+
+
+
+# Do the actuall install
+sudo apt-get -y install r-base r-base-dev
+
+# Create a symlink of texi2dvi for R CMD check, as per
+# http://stackoverflow.com/questions/10819959/diagnosing-r-package-build-warning-latex-errors-when-creating-pdf-version
+sudo ln -s /usr/bin/texi2dvi /usr/local/bin/texi2dvi
+
+# Install your favourite packages
+sudo Rscript install_r_packages.R
---
+++ os/install_r_packages.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+update.packages(ask = FALSE)
+
+install.packages(
+  c(
+    "Rcpp",
+    "dplyr",
+    "tidyr",
+    "lubridate",
+    "devtools",
+    "knitr",
+    "httr",
+    "ggplot2",
+    "NPS",
+    "arm",
+    "lme4",
+    "RPushbullet"
+  )
+)
+
+source('http://mc-stan.org/rstan/install.R', echo = TRUE, max.deparse.length = 2000)
+install_rstan()
+
