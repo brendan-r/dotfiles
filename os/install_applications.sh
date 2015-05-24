a98e158c752f38a1ef18526e9bd5edbe58b63b52
Brendan Rocks <rocks.brendan@gmail.com>
2015-05-23 17:54:49 -0700

    De OSXing

--- os/install_applications.sh
+++ os/install_applications.sh
@@ -5,16 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")" && source "utils.sh"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
-
-    declare -r OS="$(get_os)"
-
-    if [ "$OS" == "osx" ]; then
-        ./os_x/install_applications.sh
-        ./os_x/change_default_bash_version.sh
-    elif [ "$OS" == "ubuntu" ]; then
-        ./ubuntu/install_applications.sh
-    fi
-
+    ./ubuntu/install_applications.sh
 }
 
 main
