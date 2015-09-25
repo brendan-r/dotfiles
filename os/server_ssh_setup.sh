3108c60f7a1b2b8bb9d8788c0badce9ac25f5018
Brendan Rocks <rocks.brendan@gmail.com>
2015-09-24 18:38:16 -0700

    [ssh-server] Moved to apt-get part

--- os/server_ssh_setup.sh
+++ os/server_ssh_setup.sh
@@ -1,8 +1,5 @@
 # See https://help.ubuntu.com/community/SSH/OpenSSH/Configuring
 
-# Install ssh server
-sudo apt-get install openssh-server 
-
 # Back-up the settings
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.factory-defaults
 sudo chmod a-w /etc/ssh/sshd_config.factory-defaults
@@ -21,12 +18,7 @@ sudo sed -i 's/LogLevel INFO/LogLevel VERBOSE/g' /etc/ssh/sshd_config
 
 # Here you should probably set a port other than 22!
 
-# Here you should move issue.net from the dotfiles repo, over to the server
-
 # Rate limit connections
 sudo ufw limit ssh
-
-# install fail2ban: it should start running with sensible defaults automatically
-sudo apt-get install fail2ban
 
 # At this point, you need to allow port forwarding on the router...
