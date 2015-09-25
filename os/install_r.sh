49ab23db50cc15616d22211a9370ca35e45ee8f4
Brendan Rocks <rocks.brendan@gmail.com>
2015-09-24 18:40:36 -0700

    [R] Tidying

--- os/install_r.sh
+++ os/install_r.sh
@@ -1,4 +1,11 @@
-## R
+#!/bin/bash
+
+## Install R
+# Add the repo
+add-apt-repository -y "deb http://cran.rstudio.com/bin/linux/ubuntu `lsb_release -cs`/"
+# The Ubuntu archives on CRAN are signed with the key of “Michael Rutter
+# marutter@gmail.com”
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 
 # At the time of writing, this is required for git2r, which is required for
 # devtools, which is required for quite a lot of your workflow
@@ -6,24 +13,18 @@ apt-get -y build-dep libcurl4-gnutls-dev
 apt-get -y install libcurl4-gnutls-dev libssl-dev libxml2-dev mesa-common-dev \
   libcairo2-dev libxt-dev
 
- add-apt-repository -y "deb http://cran.rstudio.com/bin/linux/ubuntu `lsb_release -cs`/"
-# The Ubuntu archives on CRAN are signed with the key of “Michael Rutter
-# marutter@gmail.com”
- apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-
- apt-get update
-
+apt-get update
 
 # Do the actuall install
- apt-get -y install r-base r-base-dev
+apt-get -y install r-base r-base-dev
 
 # Create a symlink of texi2dvi for R CMD check, as per
 # http://stackoverflow.com/questions/10819959/diagnosing-r-package-build-warning-latex-errors-when-creating-pdf-version
- ln -s /usr/bin/texi2dvi /usr/local/bin/texi2dvi
+ln -s /usr/bin/texi2dvi /usr/local/bin/texi2dvi
 
 # These are needed for Cairo graphics to work
- apt-get install -y r-cran-cairodevice r-cran-kernsmooth r-cran-matrix \
-   r-cran-mgcv r-cran-nlme r-cran-nnet r-cran-rodbc
+apt-get install -y r-cran-cairodevice r-cran-kernsmooth r-cran-matrix \
+  r-cran-mgcv r-cran-nlme r-cran-nnet r-cran-rodbc
 
 # Install your favourite packages
- Rscript install_r_packages.R
+Rscript install_r_packages.R
