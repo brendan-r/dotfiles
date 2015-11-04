d5e343ececdc47defa2fc56e87f527eaec3c5583
Brendan Rocks <rocks.brendan@gmail.com>
2015-11-03 22:31:11 -0800

    [os] Add install powerline fonts sh

---
+++ install_powerline_fonts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Clone the github repo to get the files
+git clone https://github.com/powerline/fonts.git projects/powerline_fonts
+
+# Run the installer script
+bash projects/powerline_fonts/install.sh
+
