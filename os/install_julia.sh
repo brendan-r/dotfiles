2da7d0d0143fe0d8ddc3b748e0d733b841ab23b6
Brendan Rocks <rocks.brendan@gmail.com>
2015-11-03 22:30:47 -0800

    [os] Add install julia sh

---
+++ os/install_julia.sh
@@ -0,0 +1,5 @@
+# Taken from: http://julialang.org/downloads/platform.html
+sudo add-apt-repository ppa:staticfloat/juliareleases -y
+sudo add-apt-repository ppa:staticfloat/julia-deps -y
+sudo apt-get update
+sudo apt-get install julia -y
