37732cee6dbf210ddc4886fae033e159f43605b3
Brendan Rocks <rocks.brendan@gmail.com>
2016-05-16 09:16:56 -0700

    [rstudio_desktop] Tweaks (https)

--- os/install_rstudio_desktop.sh
+++ os/install_rstudio_desktop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Note: This can also be used to update Rstudio desktop (until they get a PPA!)
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Prerequisites
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -9,20 +11,23 @@ sudo apt-get -y install libjpeg62
 # Download and install the .deb file
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+# Remove any existing .deb files
+rm ~/rstudio-*-amd64.de
+
 # Get the latest version number, for 64 bit systems.
-curl http://www.rstudio.com/products/rstudio/download/ |
+curl https://www.rstudio.com/products/rstudio/download/ |
   grep -o 'https://download1.rstudio.org/rstudio.*amd64\.deb' > rs_deb_url
 
 # Download the deb file
-paste rs_deb_url |  xargs wget -q -P ~
+paste rs_deb_url |  xargs wget --quiet -P ~
 
 rm rs_deb_url
 
 sudo dpkg -i ~/rstudio-*-amd64.deb
 
-# use pandoc shipped by Rstudio for global use
-sudo ln -s /usr/lib/rstudio/bin/pandoc/pandoc /usr/local/bin
-sudo ln -s /usr/lib/rstudio/bin/pandoc/pandoc-citeproc /usr/local/bin
+# Use pandoc shipped by Rstudio for global use (ovewrite any existing files)
+sudo ln -sf /usr/lib/rstudio/bin/pandoc/pandoc /usr/local/bin
+sudo ln -sf /usr/lib/rstudio/bin/pandoc/pandoc-citeproc /usr/local/bin
 
 # Remove the .deb file
 rm ~/rstudio-*-amd64.deb
