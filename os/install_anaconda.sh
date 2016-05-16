83e4e51d3cd2bc6c3dec519e7952e44f1161b712
Brendan Rocks <rocks.brendan@gmail.com>
2016-05-16 09:15:53 -0700

    Use Anaconda 2 (not 3), for now

--- os/install_anaconda.sh
+++ os/install_anaconda.sh
@@ -3,15 +3,15 @@
 # Get the latest version for 64 bit systems
 curl https://www.continuum.io/downloads | \
     grep -o \
-    'https://[[:alnum:]|-]*.ssl.cf1.rackcdn.com/Anaconda3-.*-Linux-x86_64.sh' \
+    'https://[[:alnum:]|-]*.ssl.cf1.rackcdn.com/Anaconda2-.*-Linux-x86_64.sh' \
     >> anaconda_script_url
 
 # Download the bash file
-cat paste rs_deb_url |  xargs wget -q
+cat anaconda_script_url |  xargs wget -q
 
 # Execute it
 # You should pass the -b argument somewhere
 # Note: This script will error out if the anaconda directory already exists.
 # It's probably a good idea to detect that before running
-cat anaconda_script_url | grep -o "Anaconda3-.*-Linux-x86_64.sh" | \
+cat anaconda_script_url | grep -o "Anaconda2-.*-Linux-x86_64.sh" | \
     sed 's/$/ -b /' | xargs bash
