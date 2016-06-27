472c21cdf3cc348669cfc6096945be9107656c14
Brendan Rocks <rocksb@justin.tv>
2016-06-27 10:56:05 -0700

    [yubikey] Misc.

--- os/install_yubikey.sh
+++ os/install_yubikey.sh
@@ -8,5 +8,18 @@ sudo apt-get install -y yubikey-piv-manager          # Smartcard GUI
 sudo apt-get install -y yubikey-personalization-gui  # Personalization GUI
 # sudo apt-get install -y yubikey-piv-manager          # Personalization CLI
 
+# Install gpg2 smartcard daemon
+sudo apt-get install scdaemon
+
 # Not installed at the moment:
 # isudo apt-get install yubioath-desktop             # For laptop logins
+
+# For use with ssh -----------------------------------------------
+
+# Install pre-requisites
+sudo apt-get install -y gnupg2 gnupg-agent libpth20 pinentry-curses \
+  libccid pcscd scdaemon libksba8
+
+# This bit can't quite be automated -- you need to press 1
+gpg2 --keyserver pgp.mit.edu --search-keys brendan@brendanrocks.com
+
