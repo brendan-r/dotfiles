d8d4ca4c8b94a138988a02b03278146006e8e7bc
Brendan Rocks <rocksb@justin.tv>
2016-06-27 10:44:58 -0700

    [keyboard] Fix broken paths

--- keyboard/keyboard_setup.sh
+++ keyboard/keyboard_setup.sh
@@ -1,13 +1,13 @@
 # Note: You should replace these with symlinks...
 
-# Copy the Xmodmpa file over
-cp ~/dotfiles/keyboard/.Xmodmap ~/.Xmodmap
+# Copy the Xmodmap file over
+cp ~/projects/dotfiles/keyboard/.Xmodmap ~/.Xmodmap
 
 # Copy the main xkp file to the right pace
-cp ~/dotfiles/keyboard/us /usr/share/X11/xkb/symbols/us
+cp ~/projects/dotfiles/keyboard/us /usr/share/X11/xkb/symbols/us
 
 # Copy the basic types file
-cp ~/dotfiles/keyboard/basic /usr/share/X11/xkb/types/basic
+cp ~/projects/dotfiles/keyboard/basic /usr/share/X11/xkb/types/basic
 
 # Remove the cached files
 sudo rm -f /var/lib/xkb/*.xkm
