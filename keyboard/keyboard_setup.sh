585b0a6ee8f86d51b7cf1fad794e2f6f50118e3b
Brendan Rocks <brendan@brendanrocks.com>
2017-02-03 23:30:20 -0800

    [keyboard] Fix permissions for copying files

--- keyboard/keyboard_setup.sh
+++ keyboard/keyboard_setup.sh
@@ -4,10 +4,10 @@
 cp ~/projects/dotfiles/keyboard/.Xmodmap ~/.Xmodmap
 
 # Copy the main xkp file to the right pace
-cp ~/projects/dotfiles/keyboard/us /usr/share/X11/xkb/symbols/us
+sudo cp ~/projects/dotfiles/keyboard/us /usr/share/X11/xkb/symbols/us
 
 # Copy the basic types file
-cp ~/projects/dotfiles/keyboard/basic /usr/share/X11/xkb/types/basic
+sudo cp ~/projects/dotfiles/keyboard/basic /usr/share/X11/xkb/types/basic
 
 # Remove the cached files
 sudo rm -f /var/lib/xkb/*.xkm
