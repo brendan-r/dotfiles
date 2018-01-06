7a3fdfe6a5117c70b3bf03831e2053e1d9199b10
Brendan Rocks <brendan@brendanrocks.com>
2018-01-05 18:34:56 -0800

    [signal] Desktop installation script

---
+++ os/install_signal_desktop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Taken from https://signal.org/download/
+
+curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" |
+    sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+sudo apt update && sudo apt install -y signal-desktop
