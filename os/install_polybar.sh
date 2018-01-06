95c71902990119245c82c05c948f9886c5f5c9e2
Brendan Rocks <brendan@brendanrocks.com>
2018-01-05 17:56:25 -0800

    [polybar] Installation script

---
+++ os/install_polybar.sh
@@ -0,0 +1,16 @@
+
+# Install dependencies
+sudo apt install -y cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev \
+     libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev    \
+     libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev          \
+     libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev            \
+     libxcb-cursor-dev
+
+
+pushd ~/projects/
+git clone --recursive https://github.com/jaagr/polybar
+mkdir polybar/build
+cd polybar/build
+cmake ..
+sudo make install
+popd
