5fde570cb78e2855c0de20ecc2173262ed25c7ce
Brendan Rocks <brendan@brendanrocks.com>
2018-10-21 18:06:16 -0700

    [mime] Update mimetypes/default-applications
    - emacs25 -> emacs26
    
    - Make the script a little more robust to being run in different
      directories

--- os/set_default_apps.sh
+++ os/set_default_apps.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/bash
+ #!/usr/bin/bash
 
-ln -fs $(cd .. && pwd)/mimeapps.list \
+ln -fs $HOME/projects/dotfiles/mimeapps.list \
    $HOME/.config/mimeapps.list
 
-ln -fs $(cd .. && pwd)/mimeapps.list \
+mkd $HOME/.local/share/applications/
+
+ln -fs $HOME/projects/dotfiles/mimeapps.list \
    $HOME/.local/share/applications/mimeapps.list
