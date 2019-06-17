0090ebc48182421a72e106cba45450b1b0570c9c
Brendan Rocks <brendan@brendanrocks.com>
2019-06-17 11:46:15 -0700

    [i3] Add new deps
    These seemed to be required last time you installed it

--- os/install_i3_gaps.sh
+++ os/install_i3_gaps.sh
@@ -13,7 +13,11 @@ sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev      \
      libxcb-util0-dev libxcb-icccm4-dev libyajl-dev                          \
      libstartup-notification0-dev libxcb-randr0-dev libev-dev                \
      libxcb-cursor-dev  libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev \
-     libxkbcommon-x11-dev autoconf libxcb-xrm-dev i3status
+     libxkbcommon-x11-dev autoconf libxcb-xrm-dev i3status libxcb-shape0-dev \
+     libxcb-composite0-dev
+
+# Other stuff that you like
+sudo apt-get install -y dunst wmctrl compton i3lock xautolock
 
 # Download and build the project ---------------------------------------------
 
