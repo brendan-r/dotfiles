0148913ba111a17a39841b6e07a30605e34624e7
Brendan Rocks <brendan@brendanrocks.com>
2019-06-17 11:49:39 -0700

    [spotify] + Installation script

---
+++ os/install_spotify.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Taken from https://www.spotify.com/us/download/linux/
+
+# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# 3. Update list of available packages
+sudo apt-get update
+
+# 4. Install Spotify
+sudo apt-get install spotify-client
