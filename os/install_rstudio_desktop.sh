b036ba988a78cde24c224f614e6da36630f747f6
Brendan Rocks <brendan@brendanrocks.com>
2019-11-27 15:59:27 -0800

    [rstudio-desktop] Update for 18.04

--- os/install_rstudio_desktop.sh
+++ os/install_rstudio_desktop.sh
@@ -5,8 +5,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Prerequisites
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-sudo apt-get -y install libjpeg62 libgstreamer0.10-0 \
-  libgstreamer-plugins-base0.10-0
+sudo apt-get -y install libclang-dev gdebi-core pandoc
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Download and install the .deb file
@@ -18,15 +17,11 @@ pushd /tmp/rstudio_desktop_installation
 
 # Webscrape the dev's page for the URL of the latest version, and download it
 curl https://www.rstudio.com/products/rstudio/download/ |
-  grep -o 'https://download1.rstudio.org/rstudio.*amd64\.deb' |
-  xargs wget --quiet
+    grep -Po 'https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1\.[0-9]+\.[0-9]+-amd64\.deb' |
+    xargs wget --quiet
 
 # Install
-sudo dpkg -i rstudio-*-amd64.deb
-
-# Use pandoc shipped by Rstudio for global use (ovewrite any existing files)
-sudo ln -sf /usr/lib/rstudio/bin/pandoc/pandoc /usr/local/bin
-sudo ln -sf /usr/lib/rstudio/bin/pandoc/pandoc-citeproc /usr/local/bin
+sudo gdebi -y rstudio-*-amd64.deb
 
 # Outta here
 popd
